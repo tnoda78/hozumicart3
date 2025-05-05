ab742db00f3f9ca8f034ce22db2e28dcc7ca603a
tnoda78 <tn78adsl@gmail.com>
2025-05-05 22:26:41 +0900

    update

--- vendor/github.com/gin-gonic/gin/internal/json/json.go
+++ vendor/github.com/gin-gonic/gin/internal/json/json.go
@@ -1,16 +1,22 @@
-// Copyright 2017 Bo-Yi Wu.  All rights reserved.
+// Copyright 2017 Bo-Yi Wu. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// +build !jsoniter
+//go:build !jsoniter && !go_json && !(sonic && avx && (linux || windows || darwin) && amd64)
 
 package json
 
 import "encoding/json"
 
 var (
-	Marshal       = json.Marshal
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
 	MarshalIndent = json.MarshalIndent
-	NewDecoder    = json.NewDecoder
-	NewEncoder    = json.NewEncoder
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
 )
--- vendor/github.com/gin-gonic/gin/internal/json/jsoniter.go
+++ vendor/github.com/gin-gonic/gin/internal/json/jsoniter.go
@@ -1,17 +1,23 @@
-// Copyright 2017 Bo-Yi Wu.  All rights reserved.
+// Copyright 2017 Bo-Yi Wu. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// +build jsoniter
+//go:build jsoniter
 
 package json
 
-import "github.com/json-iterator/go"
+import jsoniter "github.com/json-iterator/go"
 
 var (
-	json          = jsoniter.ConfigCompatibleWithStandardLibrary
-	Marshal       = json.Marshal
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
+	// Marshal is exported by gin/json package.
+	Marshal = json.Marshal
+	// Unmarshal is exported by gin/json package.
+	Unmarshal = json.Unmarshal
+	// MarshalIndent is exported by gin/json package.
 	MarshalIndent = json.MarshalIndent
-	NewDecoder    = json.NewDecoder
-	NewEncoder    = json.NewEncoder
+	// NewDecoder is exported by gin/json package.
+	NewDecoder = json.NewDecoder
+	// NewEncoder is exported by gin/json package.
+	NewEncoder = json.NewEncoder
 )
